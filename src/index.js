f6579609806fe05bccf1ee49ea440bed9d5b2e69
Your Name <you@example.com>
2017-12-07 10:58:14 +0800

    login 页面搭建

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import 'antd/dist/antd.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import {
+    BrowserRouter as Router,
+    Route,
+    Switch} from 'react-router-dom'
+import App from './App';
+import Login from './component/login/login'
+
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route path="/login" component={Login}/>
+            <Route path="/"  component={App}/>
+        </Switch>
+    </Router>, document.getElementById('root'));
 registerServiceWorker();
